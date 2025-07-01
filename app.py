ed5ecf1ef5d0375434285d608a69a6584e2c6ae5
weverton alves <wevertonm22@iclou.com>
2025-06-30 21:39:18 -0300

    alteracao

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from modelos.restaurante import Restaurante
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
 
-Restaurante_praca = Restaurante('praça', 'Gourmet')
-bebida_suco = Bebida('Suco de Melancia', 5.00,'grande')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
 prato_paozinho = Prato('Paozinho', 2.00,'O melhor pao da cidade')
-Restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
-Restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 
 def main():
-    print (bebida_suco)
-    print(prato_paozinho)
+   restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
